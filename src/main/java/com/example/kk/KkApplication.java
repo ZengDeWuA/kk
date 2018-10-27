e7ffbb8227dadac6770dcd68a722a7869e2a0f73
zengdewu <1315517612@qq.com>
2018-10-27 11:26:32 +0800

    Initial commit

---
+++ src/main/java/com/example/kk/KkApplication.java
@@ -0,0 +1,12 @@
+package com.example.kk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkApplication.class, args);
+    }
+}
